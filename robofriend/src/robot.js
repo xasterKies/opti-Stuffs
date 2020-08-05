f65b0eacfe61b84ae6dfcf6a8860c26e6d5dff01
SamuelEdward <samueledward40@gmail.com>
2020-08-05 09:48:12 -0700

    robots

--- robofriend/src/robot.js
+++ robofriend/src/robot.js
@@ -1,3 +1,32 @@
 export const robots = [
-    
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Leanne',
+        email: 'Sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Kevin Graham',
+        username: 'Kevin',
+        email: 'kevin@april.biz'
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: 'Clementine',
+        email: 'clementine@april.biz'
+    },
+    {
+        id: 4,
+        name: 'Patricia Graham',
+        username: 'Patricia',
+        email: 'patricia@april.biz'
+    },
+    {
+        id: 5,
+        name: 'Chelsey Lebask',
+        username: 'Lebask',
+        email: 'lebask@april.biz'
+    },
 ]
