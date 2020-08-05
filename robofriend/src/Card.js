2a02feda9094c5709fe7155da89ceb26f0fbd9ec
SamuelEdward <samueledward40@gmail.com>
2020-08-05 10:06:57 -0700

    robo

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     return (
 
         <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
-            <img src='https://robohash.org/test?200x200' alt='robots'/>
+            <img src={'https://robohash.org/${props.id}200x200'} alt='robots'/>
             <div>
             <h2>{props.name}</h2>
             <p>{props.email}</p>
