7acbb77893517a84dc1a844af3033a7791648ae2
SamuelEdward <samueledward40@gmail.com>
2020-08-05 04:58:28 -0700

    c

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const card = () => {
     return (
         <div>
-            <img src='https://robohash.org/test' alt='robots'/>
+            <img src='https://robohash.org/test?200x200' alt='robots'/>
             <div>
             <h2>Jane Doe</h2>
             <p>jane.doe@gmail.com</p>
