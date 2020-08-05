ce5fe16b00bedaca3f79f1339b26284b30069d40
SamuelEdward <samueledward40@gmail.com>
2020-08-05 04:52:48 -0700

    card

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
 const card = () => {
-    
+    return (
+        <div>
+            <img src=''/>
+            <div>
+            <h2>Jane Doe</h2>
+            <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
 }
