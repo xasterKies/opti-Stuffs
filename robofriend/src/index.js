2acba90cf1d9d848e95147982f5c13b14a562e4b
SamuelEdward <samueledward40@gmail.com>
2020-08-05 13:52:14 -0700

    cl

--- robofriend/src/index.js
+++ robofriend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Card from './Card';
+
 import CardList from './CardList';
 import 'tachyons';
 import { robots } from './robot'
