f203b315774d062a08d7e1ce24fd1f6a08abdbf1
SamuelEdward <samueledward40@gmail.com>
2020-08-05 03:25:11 -0700

    helo

--- robofriend/src/index.js
+++ robofriend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render(<h1>Hello world</h1>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
