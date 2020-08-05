de35836c698ece21a685daf0c296f665b06a3535
SamuelEdward <samueledward40@gmail.com>
2020-08-05 12:24:10 -0700

    jsx

--- robofriend/src/Card.js
+++ robofriend/src/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-const Card = (name, email, id) => {
+const Card = ({name, email, id}) => {
     return (
         
         <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
