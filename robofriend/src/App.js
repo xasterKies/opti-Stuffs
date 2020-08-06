dd56c16bbaa0762ce99490ea5963a4c8dea65a05
SamuelEdward <samueledward40@gmail.com>
2020-08-05 17:59:31 -0700

    r

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
 import { robots } from './robot';
+import Search
 
 const App = () => {
     return (
         <div>
             <h1>RoboFriends</h1>
+            <SearchBox/>
             <CardList robots={robots}/>
         </div>
     );
