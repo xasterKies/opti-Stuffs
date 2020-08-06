652b6e8ea9ba7611a8d4df6d28ecbcdad5fbf420
SamuelEdward <samueledward40@gmail.com>
2020-08-05 19:21:27 -0700

    s

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox/>
-                <CardList robots={robots}/>
+                <CardList robots={this.state.robots}/>
             </div>
         );
     }
