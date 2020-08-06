88ab9637b6453f8a0bf8562ea90df8b9ec5f4905
SamuelEdward <samueledward40@gmail.com>
2020-08-05 18:07:28 -0700

    s

--- robofriend/src/SearchBox.js
+++ robofriend/src/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = () => {
     return(
     <div className='pa2'>
         <input
+        className='pa3 ba b--green bg-lightest-blue'
          type='search' placeholder='search robots'/>
     </div>
     );
