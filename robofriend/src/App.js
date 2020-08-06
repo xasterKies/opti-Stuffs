94b9106134ed14da0c9ed3994020efa803dbceb6
SamuelEdward <samueledward40@gmail.com>
2020-08-05 19:03:25 -0700

    a

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { robots } from './robot';
 import SearchBox from './SearchBox';
@@ -8,15 +8,19 @@ const state = {
     searchfield: ''
 }
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    );
-
+export App extends Component {
+    render() {
+        return (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox/>
+                <CardList robots={robots}/>
+            </div>
+        );
+    }
 }
+    
+
+
 
 export default App;
