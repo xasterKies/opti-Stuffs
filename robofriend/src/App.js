aef055adae73b3c7e039258a6e52a86289c0fa9f
SamuelEdward <samueledward40@gmail.com>
2020-08-07 00:44:21 -0700

    a

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -17,8 +17,6 @@ class App extends Component {
     onSearchChange = (event) => {
         this.setState({ searchfield: event.target.value })
         console.log(event.target.value)
-        
-        
     }
 
 
