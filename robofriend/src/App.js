96fcf2ea87d827d48e1c7fd0f3dded0c0a777264
SamuelEdward <samueledward40@gmail.com>
2020-09-06 01:27:39 -0700

    a

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -18,9 +18,10 @@ class App extends Component {
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=> {
-            response.json();
+            return response.json();
         }).then(users => { 
-        .this.setState({ robots: robots})
+        this.setState({ robots: users})
+     })
     }
 })
 
