2d34ebd2224d904dcf7f470a4d052af13ae1d4b6
TiokengSamuel <samueledward40@gmail.com>
2020-10-29 10:49:43 +0100

    scroll

--- robofriend/src/Scroll.js
+++ robofriend/src/Scroll.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Scroll = () => {
+export const Scroll = (props) => {
     return (
-        
-    )
+        <div style= {{overflowY: 'scroll', border: '1px solid black' ,}}>
+            {props.children}
+        </div>
+    );
 }
