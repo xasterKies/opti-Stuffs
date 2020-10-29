b8302279e95889457663b0d02b4b98b37f698f05
TiokengSamuel <samueledward40@gmail.com>
2020-10-29 11:08:08 +0100

    scroll

--- robofriend/src/components/Scroll.js
+++ robofriend/src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scroll = (props) => {
     return (
-        <div style= {{overflowY: 'scroll', border: '5px solid black', height: '500px'}}>
+        <div style= {{overflowY: 'scroll', border: '1px solid #eee', height: '500px'}}>
             {props.children}
         </div>
     );
