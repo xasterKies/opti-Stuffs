705057e3b54e95947a6b3522deeba19012215c29
TiokengSamuel <samueledward40@gmail.com>
2020-10-29 12:02:00 +0100

    app

--- robofriend/src/containers/App.js
+++ robofriend/src/containers/App.js
@@ -45,8 +45,9 @@ class App extends Component {
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
                 <Scroll>
-                    
-                    <CardList robots={filteredRobots}/>
+                    <ErrorBoundry>
+                        <CardList robots={filteredRobots}/>
+                    </ErrorBoundry>
                 </Scroll>
                 
             </div>
