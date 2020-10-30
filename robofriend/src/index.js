32f3a51c9c9f680f27f7f33b212138599302f84b
TiokengSamuel <samueledward40@gmail.com>
2020-10-30 12:58:46 +0100

    react0redux

--- robofriend/src/index.js
+++ robofriend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider, connect }
+import { Provider } from 'react-redux'
 import * as serviceWorker from './serviceWorker';
 import App from './containers/App';
 import 'tachyons';
