1568cd56604c6f9f674dad6f8eefefed06c4abdd
TiokengSamuel <samueledward40@gmail.com>
2020-10-30 16:35:26 +0100

    map

--- robofriend/src/containers/App.js
+++ robofriend/src/containers/App.js
@@ -16,7 +16,7 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    return onSearchChange: (event) => dispatch(setSearchField(event.target.value))
 }
 
 class App extends Component {
