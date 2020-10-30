754f6db36632fc6ec20f83599a3ef318c044e937
TiokengSamuel <samueledward40@gmail.com>
2020-10-30 12:45:20 +0100

    reducer

--- robofriend/src/reducers.js
+++ robofriend/src/reducers.js
@@ -8,5 +8,7 @@ export const searchRobots = (state = initialState, action={}) => {
     switch(action.type) {
         case: CHANGE_SEARCH_FIELD:
             return Object.assign({}, state, searchField: action.payload)
+        default:
+            return state;
     }
 }
