82fd22316dda21946bcdc2307a9617a8059ba568
TiokengSamuel <samueledward40@gmail.com>
2020-10-30 18:46:20 +0100

    app

--- robofriend/src/containers/App.js
+++ robofriend/src/containers/App.js
@@ -42,16 +42,16 @@ class App extends Component {
 
     render() {
         const { robots } = this.state;
-        const { searchField } = this.props;
+        const { searchField, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         })
          return !robots.length ?
          <h1>Loading</h1> :
           (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={filteredRobots}/>
