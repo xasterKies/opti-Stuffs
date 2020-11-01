2bf21a4d50e0ee05a81e2cd7967edd54951fb51f
TiokengSamuel <samueledward40@gmail.com>
2020-11-01 01:34:56 +0100

    search

--- robofriend/src/containers/App.js
+++ robofriend/src/containers/App.js
@@ -37,6 +37,10 @@ class App extends Component {
         this.setState({ robots: users})
      })
     }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
     
 
     render() {
