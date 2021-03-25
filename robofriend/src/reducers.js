e5b81c47de2737f24634dc12f2f6a32e580d094f
TiokengSamuel <samueledward40@gmail.com>
2021-03-25 17:37:11 +0100

    advanced states

--- robofriend/src/reducers.js
+++ robofriend/src/reducers.js
@@ -1,4 +1,10 @@
-import { CHANGE_SEARCH_FIELD} from  './constants'
+import { 
+    CHANGE_SEARCH_FIELD,
+    REQUEST_ROBOTS_PENDING,
+    REQUEST_ROBOTS_SUCCESS,
+    REQUEST_ROBOTS_FAILED
+
+} from  './constants'
 
 const initialState = {
     searchField: ''
@@ -10,5 +16,14 @@ export const searchRobots = (state = initialState, action={}) => {
             return Object.assign({}, state, { searchField: action.payload })
         default:
             return state;
+    }
+}
+
+export const requestRobots = (state=initialState, action={}) => {
+    switch(action.type) {
+        case REQUEST_ROBOTS_PENDING:
+            return Object.assign({}, state, { isPending: true})
+        case REQUEST_ROBOTS_SUCCESS:
+                return Object.assign({})
     }
 }
