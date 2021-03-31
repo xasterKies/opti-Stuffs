8b6be81a44d3e1a3b9149596e3a370a181bd55d1
TiokengSamuel <samueledward40@gmail.com>
2021-03-31 14:59:45 +0100

    setting up page 1

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Page1 from './components/Page1' 
+import Page2 from './components/Page2' 
+import Page3 from './components/Page3' 
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     <h1>Hello pages</h1>
+     <Page1/>
+     <Page2/>
+     <Page3/>
     </div>
   );
 }
---
+++ code-splitting/src/components/Page1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logo from '../logo.svg'
+
+const Page1 = () => 
+    <div className="App">
+        <h1>Page 1</h1>
+        <img src={logo} alt=""/>
+        <button>Page 2</button>
+        <button>Page 3</button>
+    </div>
