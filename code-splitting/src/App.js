5d8b6023ef074ce0e8622378ddfb4621a02a6d66
TiokengSamuel <samueledward40@gmail.com>
2021-03-31 15:08:06 +0100

    routing logic in the constructor

--- code-splitting/src/App.js
+++ code-splitting/src/App.js
@@ -5,16 +5,28 @@ import Page2 from './components/Page2'
 import Page3 from './components/Page3' 
 
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
+  render() {
   return (
     <div className="App">
 
      <h1>Hello pages</h1>
-     <Page1/>
-     <Page2/>
-     <Page3/>
+     <Page1 onRouteChange={this.onRouteChange}/>
+     <Page2 onRouteChange={this.onRouteChange}/>
+     <Page3 onRouteChange={this.onRouteChange}/>
     </div>
   );
+}
 }
 
 export default App;
